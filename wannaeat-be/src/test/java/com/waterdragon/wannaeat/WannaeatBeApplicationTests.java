16dea6610cd5bab5283b4d0ef7a3759438af9c53
jeonghyeon <wjd4782@naver.com>
2024-09-10 13:04:26 +0900

    init: 백엔드 프로젝트 초기화

---
+++ wannaeat-be/src/test/java/com/waterdragon/wannaeat/WannaeatBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waterdragon.wannaeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WannaeatBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

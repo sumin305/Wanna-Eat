96d6df9152ee1f88b7de75e09dc88c7442daf6b2
sumin305 <gogotnals@naver.com>
2024-09-12 00:13:36 +0900

    refactor: 모달 버튼 WEButton 컴포넌트로 대체

--- wannaeat-fe/src/component/common/modal/WEAlertModal/WEAlertModal.jsx
+++ wannaeat-fe/src/component/common/modal/WEAlertModal/WEAlertModal.jsx
@@ -1,25 +1,13 @@
-/* eslint-disable react/no-unknown-property */
-/* eslint-disable react/prop-types */
-/** @jsxImportSource @emotion/react */
-
-import { css } from '@emotion/react';
-import { theme } from '../../../../style/common/theme';
 import {ModalStyled, TitleStyled, ButtonContainerStyled} from './WEAlertModal'
+import WEButton from '../../button/WEButton/WEButton'
 
-// 대리우스의 버튼 컴포넌트로 변경 예정
-const Button = ({ text, onClick, textColor, backgroundColor}) => (
-  <button css={buttonStyle(textColor, backgroundColor)}
-  onClick={onClick}>
-    {text}
-  </button>
-)
 const WEAlertModal = ({ alertText, cancelText, confirmText, close }) => {
   return (
     <ModalStyled>
       <TitleStyled>{alertText}</TitleStyled>
       <ButtonContainerStyled>
-        <Button text={cancelText} onClick={close} textColor={'black'} backgroundColor={'E6EAEE'}/>
-        <Button text={confirmText} onClick={close} textColor={'white'} backgroundColor={theme.colors.primary}/>
+        <WEButton size="short" onClick={close} backgroundColor={'E6EAEE'} color={'black'}>{cancelText}</WEButton>
+        <WEButton size="short" onClick={close}>{confirmText}</WEButton>
       </ButtonContainerStyled>
     </ModalStyled>
   );

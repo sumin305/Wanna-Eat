f386ddcdc8c16cac74b2c1e7bb0d91bbebb21ce0
sumin305 <gogotnals@naver.com>
2024-09-11 23:39:08 +0900

    feat: 컴포넌트 테스트를 위한 TopPage 생성

--- wannaeat-fe/src/component/common/modal/TopPage.jsx
+++ wannaeat-fe/src/component/common/modal/TopPage.jsx
@@ -1,2 +1,32 @@
-import {React} from "react";
+import { React, useState } from 'react';
+import WEAlertModal from './WEAlertModal/WEAlertModal';
+import WEBlackOutStyle from './WEBlackOutStyle';
+import visibleStore from './visibleStore';
+
+const TopPage = () => {
+  const {isVisible, open, close}= visibleStore();
+
+  return (
+    <div>
+      <div>
+        <button onClick={open}>모달 띄우기</button>
+      </div>
+      <div>
+        {isVisible && (
+          <WEBlackOutStyle close={close}></WEBlackOutStyle>
+        )}
+        {isVisible && (
+          <WEAlertModal
+            alertText='삭제하시겠습니까?'
+            cancelText='취소'
+            confirmText='확인'
+            open={open} close={close}
+          ></WEAlertModal>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TopPage;
 

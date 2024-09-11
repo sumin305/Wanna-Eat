220f7c98f7a7d0723b8c2e2b97917e380417c483
sumin305 <gogotnals@naver.com>
2024-09-11 23:45:21 +0900

    feat: 경고 알림 모달 구현

---
+++ wannaeat-fe/src/component/common/modal/WEAlertModal/WEAlertModal.js
@@ -0,0 +1,29 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/prop-types */
+/** @jsxImportSource @emotion/react */
+
+import { React } from 'react';
+import { css } from '@emotion/react';
+import { theme } from '../../../../style/common/theme';
+import {modalStyle, titleStyle, buttonContainerStyle, buttonStyle} from './WEModalStyle'
+
+// 대리우스의 버튼 컴포넌트로 변경 예정
+const Button = ({ text, onClick, textColor, backgroundColor}) => (
+  <button css={buttonStyle(textColor, backgroundColor)}
+  onClick={onClick}>
+    {text}
+  </button>
+)
+const WEAlertModal = ({ alertText, cancelText, confirmText, close }) => {
+  return (
+    <div css={modalStyle}>
+      <div css={titleStyle}>{alertText}</div>
+      <div css={buttonContainerStyle}>
+        <Button text={cancelText} onClick={close} textColor={'black'} backgroundColor={'E6EAEE'}/>
+        <Button text={confirmText} onClick={close} textColor={'white'} backgroundColor={theme.colors.primary}/>
+      </div>
+    </div>
+  );
+};
+
+export default WEAlertModal;

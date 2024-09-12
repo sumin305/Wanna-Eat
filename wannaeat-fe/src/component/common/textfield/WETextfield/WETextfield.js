3f6f05717a05678312374e903a22fa7e1f601f96
이정현 <gpwmfsjt3@naver.com>
2024-09-12 17:57:41 +0900

    feat: default 스타일 추가

--- wannaeat-fe/src/component/common/textfield/WETextfield/WETextfield.js
+++ wannaeat-fe/src/component/common/textfield/WETextfield/WETextfield.js
@@ -1,6 +1,15 @@
 import styled from '@emotion/styled';
 // import theme from '../../../../style/common/theme';
 
-const TextfieldStyled = styled.input``;
+const TextfieldStyled = styled.input`
+  width: 15.3125rem;
+  height: 2rem;
+  border: 1px solid #606060;
+  font-size: 0.6875rem;
+  font-weight: 500;
+
+  :focus {
+  }
+`;
 
 export default TextfieldStyled;

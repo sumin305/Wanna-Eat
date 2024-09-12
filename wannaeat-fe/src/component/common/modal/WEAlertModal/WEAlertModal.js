13edba715da0e9bd003fea59e91428b97d6ece4b
이수민 <gogotnals@naver.com>
2024-09-12 10:33:52 +0900

    design: theme 설정

--- wannaeat-fe/src/component/common/modal/WEAlertModal/WEAlertModal.js
+++ wannaeat-fe/src/component/common/modal/WEAlertModal/WEAlertModal.js
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
-import { theme } from '../../../../style/common/theme';
+import theme from '../../../../style/common/theme';
 
 const ModalStyled = styled.div`
   display: grid;
   grid-template-rows: 1fr 1fr 1fr 1fr 1fr 2fr 0.5fr;
   position: fixed;
   background-color: white;
-  border-radius: 0.3rem;
+  border-radius: ${theme.borderRadius.default};
   padding: 0.5rem;
   width: 70%;
   height: 20%;
@@ -15,20 +15,19 @@ const ModalStyled = styled.div`
   top: 50%;
   transform: translate(-50%, -50%);
   z-index: ${theme.zIndex.modal};
-`
+`;
 
 const TitleStyled = styled.div`
-  font-size: 0.813rem;
+  font-size: ${theme.fontSize.px13}rem;
   margin-top: 10px;
   text-align: center;
   grid-row: 3;
-  font-weight: 500;
-`
+`;
 
 const ButtonContainerStyled = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-row: 6;
-`
+`;
 
-export {ModalStyled, TitleStyled, ButtonContainerStyled};
+export { ModalStyled, TitleStyled, ButtonContainerStyled };

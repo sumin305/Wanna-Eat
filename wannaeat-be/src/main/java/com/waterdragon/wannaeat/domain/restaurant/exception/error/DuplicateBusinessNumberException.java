933423f1f1cdb57e92f68efb871a5756b20567d5
이경곤 <leeggon1213@naver.com>
2024-09-13 13:49:30 +0900

    feat: 매장 등록시 예외 구현

---
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurant/exception/error/DuplicateBusinessNumberException.java
@@ -0,0 +1,5 @@
+package com.waterdragon.wannaeat.domain.restaurant.exception.error;
+
+public class DuplicateBusinessNumberException extends RuntimeException {
+	public DuplicateBusinessNumberException(String message) { super(message); }
+}

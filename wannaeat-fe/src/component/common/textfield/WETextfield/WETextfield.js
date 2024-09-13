039da6f636ab74fdd9aabf6b141cbce2877c13be
이정현 <gpwmfsjt3@naver.com>
2024-09-13 15:03:43 +0900

    feat: 에러 메시지, 아이콘 추가

--- wannaeat-fe/src/component/common/textfield/WETextfield/WETextfield.js
+++ wannaeat-fe/src/component/common/textfield/WETextfield/WETextfield.js
@@ -3,20 +3,45 @@ import theme from '../../../../style/common/theme';
 
 const TextfieldStyled = styled.input`
   width: ${theme.width.textfield};
-  height: ${theme.height};
-  border: 1px solid ${props => (props.error ? theme.color.warning : '#606060')};
+  height: ${theme.height.textfield};
+  border: 1px solid
+    ${(props) => (props.error ? theme.color.warning : '#606060')};
   font-weight: 500;
-  color: ${props => (props.error ? theme.color.warning : '#606060')};
-  padding: 10px 24px;
+  color: ${(props) => (props.error ? theme.color.warning : '#606060')};
+  padding: 0.625rem 1.5rem;
   font-size: ${theme.fontSize.px11};
+
+  box-sizing: border-box;
 
   outline: none;
 
   &:focus {
-    border: 1px solid ${theme.color.primary}; 
-    box-shadow: 0px 1px 1px 0px rgba(0, 0, 0, 0.50);
+    border: 1px solid ${theme.color.primary};
+    box-shadow: 0px 1px 1px 0px rgba(0, 0, 0, 0.5);
     color: ${theme.color.primary};
+  }
+
+  &:focus::placeholder {
+    color: transparent;
   }
 `;
 
-export default TextfieldStyled;
+const ErrorMessageStyled = styled.div`
+  color: ${theme.color.warning};
+  font-size: 0.75rem;
+`;
+
+const ErrorMessageDivStyled = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0.313rem;
+
+  svg {
+    width: 1000px;
+    height: 1000px;
+  }
+`;
+
+export default { TextfieldStyled, ErrorMessageStyled, ErrorMessageDivStyled };
--- wannaeat-fe/src/component/common/textfield/WETextfield/WETextfield.jsx
+++ wannaeat-fe/src/component/common/textfield/WETextfield/WETextfield.jsx
@@ -1,13 +1,30 @@
-import TextfieldStyled from "./WETextfield.js";
-import ErrorMessageStyled from "./WEErrorMessage.js";
+import { ReactComponent as WarningIcon } from '../../../../assets/warning.svg';
+import components from './WETextfield.js';
+const { TextfieldStyled, ErrorMessageStyled, ErrorMessageDivStyled } =
+  components;
 
-const Textfield = ({type="text", error, errorMessage, ...props}) => {
-    return (
-        <div>
-            <TextfieldStyled type={type} error={error} {...props} />
-            {error && <ErrorMessageStyled>{errorMessage}</ErrorMessageStyled>}
-        </div>
-    );
+const Textfield = ({
+  type = 'text',
+  error = false,
+  errorMessage = '',
+  ...props
+}) => {
+  return (
+    <div>
+      <TextfieldStyled
+        type={type}
+        error={error}
+        errorMessage={errorMessage}
+        {...props}
+      />
+      {error && (
+        <ErrorMessageDivStyled>
+          {<WarningIcon />}
+          {<ErrorMessageStyled>{errorMessage}</ErrorMessageStyled>}
+        </ErrorMessageDivStyled>
+      )}
+    </div>
+  );
 };
 
 export default Textfield;

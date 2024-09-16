451c64edb28626c05cfebc0c6eef8956277cc753
이경곤 <leeggon1213@naver.com>
2024-09-16 22:32:10 +0900

    feat: RestaurantRepository findByRestaurantIdAndUser 메소드, findByBusinessNumberAndRestaurantIdNot 메소드 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurant/repository/RestaurantRepository.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurant/repository/RestaurantRepository.java
@@ -5,8 +5,13 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.waterdragon.wannaeat.domain.restaurant.domain.Restaurant;
+import com.waterdragon.wannaeat.domain.user.domain.User;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
-	Optional<Restaurant> findByBusinessNumber(String BusinessNumber);
+	Optional<Restaurant> findByRestaurantIdAndUser(Long restaurantId, User user);
+
+	Optional<Restaurant> findByBusinessNumber(String businessNumber);
+
+	Optional<Restaurant> findByBusinessNumberAndRestaurantIdNot(String businessNumber, Long restaurantId);
 }

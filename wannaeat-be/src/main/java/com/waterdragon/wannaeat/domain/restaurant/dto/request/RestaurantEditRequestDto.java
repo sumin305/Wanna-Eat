0a9defb2bdfc1a507f04be1e6c9837fa27c994c9
이경곤 <leeggon1213@naver.com>
2024-09-16 22:34:40 +0900

    feat: RestaurantEditRequestDto 구현

---
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurant/dto/request/RestaurantEditRequestDto.java
@@ -0,0 +1,60 @@
+package com.waterdragon.wannaeat.domain.restaurant.dto.request;
+
+import java.time.LocalTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class RestaurantEditRequestDto {
+
+	@NotEmpty(message = "사업자 번호는 필수 입력값입니다.")
+	private String restaurantBusinessNumber;
+
+	@NotEmpty(message = "매장 주인은 필수 입력값입니다.")
+	private String restaurantOwnerName;
+
+	@NotEmpty(message = "매장 주소는 필수 입력값입니다.")
+	private String restaurantAddress;
+
+	@NotEmpty(message = "매장 번호는 필수 입력값입니다.")
+	@Size(min = 11, max = 11, message = "매장 번호는 11자리여야 합니다.")
+	private String restaurantPhone;
+
+	@NotEmpty(message = "매장 이름은 필수 입력값입니다.")
+	private String restaurantName;
+
+	@NotNull(message = "매장 카테고리는 필수 입력값입니다.")
+	private Long restaurantCategoryId;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+	private LocalTime restaurantOpenTime;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+	private LocalTime restaurantCloseTime;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+	private LocalTime breakStartTime;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+	private LocalTime breakEndTime;
+
+	private Integer maxReservationTime;
+
+	private Integer minMemberCount;
+
+	private Integer maxMemberCount;
+
+	private Integer depositPerMember;
+
+	private String restaurantDescription;
+
+	private Double latitude;
+
+	private Double longitude;
+}

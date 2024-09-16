33d0f1935b8cc72969715c7301bf0a423c2e7bb8
곽예빈 <ferf99@naver.com>
2024-09-17 04:21:47 +0900

    feat: userRepository 이메일, 소셜타입 검색 메서드 추가

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/user/repository/UserRepository.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/user/repository/UserRepository.java
@@ -5,8 +5,15 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.waterdragon.wannaeat.domain.user.domain.User;
+import com.waterdragon.wannaeat.domain.user.domain.enums.SocialType;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByUserIdAndDeletedFalse(Long id);
+
+	Optional<User> findByEmail(String email);
+
+	Optional<User> findByEmailAndSocialType(String email, SocialType socialType);
+
+	Optional<User> findByNickname(String nickname);
 }

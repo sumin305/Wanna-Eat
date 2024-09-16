992bbd59f762f0dc67458f99b9adf1fc92498daa
이경곤 <leeggon1213@naver.com>
2024-09-16 22:34:07 +0900

    feat: InvalidRestaurantTimeException 구현

---
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurant/exception/error/InvalidRestaurantTimeException.java
@@ -0,0 +1,5 @@
+package com.waterdragon.wannaeat.domain.restaurant.exception.error;
+
+public class InvalidRestaurantTimeException extends RuntimeException {
+	public InvalidRestaurantTimeException(String message) {	super(message);	}
+}

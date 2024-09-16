b16be8eca649a70b224cbd8f44901598d9b0e402
이경곤 <leeggon1213@naver.com>
2024-09-16 22:33:58 +0900

    feat: InvalidBreakTimeException 구현

---
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurant/exception/error/InvalidBreakTimeException.java
@@ -0,0 +1,5 @@
+package com.waterdragon.wannaeat.domain.restaurant.exception.error;
+
+public class InvalidBreakTimeException extends RuntimeException {
+	public InvalidBreakTimeException(String message) {	super(message);	}
+}

85982f134eea2f6cc2c5f6118df57b5da31329ee
박소정 <thwjd0235@naver.com>
2024-09-16 14:44:54 +0900

    feat: 헤더에 뒤로가기 아이콘이나 로고가 없을 때 pageName이 왼쪽으로 가도록 추가

--- wannaeat-fe/src/layout/common/WEHeader/WEHeader.js
+++ wannaeat-fe/src/layout/common/WEHeader/WEHeader.js
@@ -16,10 +16,43 @@ const HeaderContainer = styled.div`
 
 const HeaderWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   width: 90%;
   height: 100%;
+  position: relative;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  position: absolute;
+  left: 0;
+  width: ${(props) => (props.isShowBackIcon ? '10%' : '0%')};
+`;
+
+const HeaderCenter = styled.div`
+  display: flex;
+  justify-content: ${(props) => {
+    return props.children.props.isShowBackIcon ||
+      props.children.props.isShowLogo
+      ? 'center'
+      : 'flex-start';
+  }};
+  width: ${(props) => {
+    return props.children.props.isShowBackIcon ||
+      props.children.props.isShowLogo
+      ? '80%'
+      : '90%';
+  }};
+`;
+
+const HeaderRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  position: absolute;
+  right: 0;
+  width: 10%;
 `;
 
 const HeaderImg = styled.img`
@@ -39,4 +72,13 @@ const IconImg = styled.img`
   height: 1.5rem;
 `;
 
-export { HeaderContainer, HeaderWrapper, HeaderImg, HeaderTitle, IconImg };
+export {
+  HeaderContainer,
+  HeaderWrapper,
+  HeaderLeft,
+  HeaderCenter,
+  HeaderRight,
+  HeaderImg,
+  HeaderTitle,
+  IconImg,
+};

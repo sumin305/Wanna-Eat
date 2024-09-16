d1d6a33e985a2e674c53315ed14df0c1b637c1dd
박소정 <thwjd0235@naver.com>
2024-09-16 23:09:26 +0900

    refactor: 서브헤더 zustand로 관리하도록 변경

--- wannaeat-fe/src/pages/manager/main/MainPage.jsx
+++ wannaeat-fe/src/pages/manager/main/MainPage.jsx
@@ -1,9 +1,15 @@
+import { useEffect } from 'react';
 import WEHeader from '../../../layout/common/WEHeader/WEHeader.jsx';
 import WEExtraHeader from '../../../layout/common/WEHeader/WEExtraHeader.jsx';
 import useHeaderStore from '../../../stores/header/useHeaderStore.js';
 
 const MainPage = () => {
-  const { icons } = useHeaderStore();
+  const { icons, isShowExtraHeader, setIsShowExtraHeader } = useHeaderStore();
+
+  useEffect(() => {
+    setIsShowExtraHeader(true); //페이지가 로드될 때 ExtraHeader 보여줌
+    return () => setIsShowExtraHeader(false); //페이지가 언마운트될 때 ExtraHeader 숨김
+  }, [setIsShowExtraHeader]);
 
   return (
     <>
@@ -15,7 +21,9 @@ const MainPage = () => {
         isShowBackIcon={true}
       />
 
-      <WEExtraHeader isExtraCarrot={true} text="등록증 스캔" />
+      {isShowExtraHeader && (
+        <WEExtraHeader isExtraCarrot={true} text="등록증" />
+      )}
 
       <div>사장님 메인페이지</div>
     </>

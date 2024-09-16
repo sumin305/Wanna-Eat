79127c14057da9b2f1f6c71fc75c6bbf26dbb061
이경곤 <leeggon1213@naver.com>
2024-09-17 02:54:56 +0900

    feat: MenuRepository findByMenuIdAndDeletedFalse 메소드 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/menu/repository/MenuRepository.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/menu/repository/MenuRepository.java
@@ -1,8 +1,12 @@
 package com.waterdragon.wannaeat.domain.menu.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.waterdragon.wannaeat.domain.menu.domain.Menu;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+	Optional<Menu> findByMenuIdAndDeletedFalse(Long MenuId);
 }

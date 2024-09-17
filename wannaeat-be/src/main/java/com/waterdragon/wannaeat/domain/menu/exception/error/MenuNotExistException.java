55639b0557adafe08e45904543290abd88a22469
이경곤 <leeggon1213@naver.com>
2024-09-17 12:33:14 +0900

    rename: MenuNotFoundException 리네임

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/menu/exception/error/MenuNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.waterdragon.wannaeat.domain.menu.exception.error;
-
-public class MenuNotExistException extends RuntimeException {
-	public MenuNotExistException(String message) {
-		super(message);
-	}
-}

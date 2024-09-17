54fb9361afb3dfb3259f9f8234e154a41a69c3ec
박소정 <thwjd0235@naver.com>
2024-09-17 18:03:40 +0900

    fix: 탭바 위치 수정

--- wannaeat-fe/src/stores/common/useCommonStore.js
+++ wannaeat-fe/src/stores/common/useCommonStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 
 const useCommonStore = create((set) => ({
-  isManager: false, //기본값은 true로 사장님
+  isManager: true, //기본값은 true로 사장님
 
   // isManager의 값을 변경시키는 함수 (추후 유저의 role이 "MANAGER"인지 "CUSTOMER"인지에 따라 바꾸면 될듯)
   setIsManager: (value) => set(() => ({ isManager: value })),

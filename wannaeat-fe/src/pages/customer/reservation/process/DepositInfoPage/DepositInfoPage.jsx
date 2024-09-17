cb8b5d6a05671d1f282c67e8da911173fcb78f56
sumin305 <gogotnals@naver.com>
2024-09-18 02:44:28 +0900

    feat: DepositInfoPage 레이아웃 구현

--- wannaeat-fe/src/pages/customer/reservation/process/DepositInfoPage/DepositInfoPage.jsx
+++ wannaeat-fe/src/pages/customer/reservation/process/DepositInfoPage/DepositInfoPage.jsx
@@ -1,7 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import WEStep from "../../../../../component/customer/reservation/WEStep/WEStep.jsx";
+import { ButtonWrapper } from "../TimeSelectPage/TimeSelectPage";
+import theme from "../../../../../style/common/theme.js";
+import Button from "../../../../../component/common/button/WEButton/WEButton.jsx";
 const DepositInfoPage = () => {
+    const navigate = useNavigate();
+    const price = 50000;
+    const handleBeforeButtonClick = () => {
+        navigate('/customer/reservation/time-select')
+    }
+    const handleNextButtonClick = () => {
+        navigate('/customer/reservation/deposit-payment')
+    }
     return(
-        <div></div>
+        <div>
+            <WEStep index={2}/>
+            <div><p>{price}</p> <p>원</p></div>
+            <div>결제해주세요.</div>
+            <ButtonWrapper>
+                <Button onClick={handleBeforeButtonClick} size="short" color={"black"} backgroundColor={theme.color.disabled}>이전</Button>
+                <Button onClick={handleNextButtonClick} size="venti">예약</Button>
+            </ButtonWrapper>
+        </div>
     )
+
 }
 
 export default DepositInfoPage;

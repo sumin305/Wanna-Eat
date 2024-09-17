237605e293a73f37b473b0966f93acc03819227c
이경곤 <leeggon1213@naver.com>
2024-09-17 12:32:54 +0900

    rename: InvalidRestaurantOpenCloseTimeException 리네임

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurant/exception/error/InvalidRestaurantTimeException.java
+++
@@ -1,7 +0,0 @@
-package com.waterdragon.wannaeat.domain.restaurant.exception.error;
-
-public class InvalidRestaurantTimeException extends RuntimeException {
-	public InvalidRestaurantTimeException(String message) {
-		super(message);
-	}
-}

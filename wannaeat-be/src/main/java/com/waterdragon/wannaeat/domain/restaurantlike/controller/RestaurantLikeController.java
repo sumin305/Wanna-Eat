27e93b31a406286498438490934f1cd77fa72652
이경곤 <leeggon1213@naver.com>
2024-09-20 17:54:08 +0900

    feat: 찜 매장 목록 조회 Controller 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurantlike/controller/RestaurantLikeController.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurantlike/controller/RestaurantLikeController.java
@@ -3,11 +3,13 @@ package com.waterdragon.wannaeat.domain.restaurantlike.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.waterdragon.wannaeat.domain.restaurantlike.dto.response.UserLikeListResponseDto;
 import com.waterdragon.wannaeat.domain.restaurantlike.service.RestaurantLikeService;
 import com.waterdragon.wannaeat.global.response.ResponseDto;
 
@@ -43,6 +45,27 @@ public class RestaurantLikeController {
 
 	}
 
+	@Operation(summary = "찜 매장 목록 조회 API")
+	@GetMapping("/users/like")
+	ResponseEntity<ResponseDto<UserLikeListResponseDto>> getListUserLikeRestaurant() {
+
+		UserLikeListResponseDto userLikeListResponseDto = restaurantLikeService.getListUserLikeRestaurant();
+		ResponseDto<UserLikeListResponseDto> responseDto = ResponseDto.<UserLikeListResponseDto>builder()
+			.status(HttpStatus.OK.value())
+			.message("찜한 매장 목록이 성공적으로 조회되었습니다.")
+			.data(userLikeListResponseDto)
+			.build();
+
+		return new ResponseEntity<>(responseDto, HttpStatus.OK);
+
+	}
+
+	/**
+	 * 매장 찜 삭제 API
+	 *
+	 * @param restaurantId 매장 id
+	 * @return void 매장 삭제 결과
+	 */
 	@Operation(summary = "매장 찜 삭제 API")
 	@DeleteMapping("/restaurants/{restaurantId}/like")
 	ResponseEntity<ResponseDto<Void>> removeRestaurantLike(

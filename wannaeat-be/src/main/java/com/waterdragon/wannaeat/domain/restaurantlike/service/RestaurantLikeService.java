08b6e5189edf63b2c843f37b25e13b2ce8ef2b59
이경곤 <leeggon1213@naver.com>
2024-09-20 15:48:51 +0900

    feat: 매장 찜 삭제 Service 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurantlike/service/RestaurantLikeService.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurantlike/service/RestaurantLikeService.java
@@ -3,4 +3,6 @@ package com.waterdragon.wannaeat.domain.restaurantlike.service;
 public interface RestaurantLikeService {
 
 	void registerRestaurantLike(Long restaurantId);
+
+	void removeRestaurantLike(Long restaurantId);
 }

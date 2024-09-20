b77376a2c5248e912e33f1da45ec933b665fa264
박소정 <thwjd0235@naver.com>
2024-09-20 13:52:25 +0900

    feat: 현재 위치를 중심으로 한 지도 생성

--- wannaeat-fe/src/component/customer/map/Map.js
+++ wannaeat-fe/src/component/customer/map/Map.js
@@ -1,9 +1,11 @@
 import styled from '@emotion/styled';
 
 const MapView = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100%;
-  touch-action: none; /* 터치 이벤트 처리 */
   overflow: hidden;
 `;
 
--- wannaeat-fe/src/component/customer/map/Map.jsx
+++ wannaeat-fe/src/component/customer/map/Map.jsx
@@ -1,21 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapView } from './Map';
 
 const MapContainer = () => {
+  const [lat, setLat] = useState(33.450701); // 위도값
+  const [lon, setLon] = useState(126.570667); // 경도값
+
   useEffect(() => {
     const { kakao } = window;
     const container = document.getElementById('map'); // 지도를 표시할 div
     const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+      center: new kakao.maps.LatLng(lat, lon), // 지도의 중심좌표
       level: 3, // 지도의 확대 레벨
     };
+
     const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
 
-    // 지도 컨테이너의 크기가 변경되었을 때 호출
-    window.addEventListener('resize', () => {
-      map.relayout();
-    });
-  }, []);
+    // 현재 위치에 해당하는 위도, 경도값으로 변경
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLon(position.coords.longitude);
+      });
+    }
+  }, [lat, lon]);
 
   return <MapView id="map"></MapView>;
 };

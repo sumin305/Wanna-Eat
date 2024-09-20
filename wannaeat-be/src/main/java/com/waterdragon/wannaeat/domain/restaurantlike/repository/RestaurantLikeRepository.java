77b27c31515600d7661ee637ff4246f4501c2975
이경곤 <leeggon1213@naver.com>
2024-09-20 17:51:50 +0900

    feat: RestaurantLikeRepository findByUser 메소드 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurantlike/repository/RestaurantLikeRepository.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurantlike/repository/RestaurantLikeRepository.java
@@ -1,5 +1,6 @@
 package com.waterdragon.wannaeat.domain.restaurantlike.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ import com.waterdragon.wannaeat.domain.user.domain.User;
 public interface RestaurantLikeRepository extends JpaRepository<RestaurantLike, Long> {
 
 	Optional<RestaurantLike> findByUserAndRestaurant(User user, Restaurant restaurant);
+
+	List<RestaurantLike> findByUser(User user);
 }

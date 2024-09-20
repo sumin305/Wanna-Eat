030fafbdcffc6f6e031613c7caa6774e3785ed3d
박소정 <thwjd0235@naver.com>
2024-09-21 03:03:37 +0900

    rename: stores/header를 stores/common/header로 이동

--- wannaeat-fe/src/layout/common/WEHeader/WEExtraHeader.jsx
+++ wannaeat-fe/src/layout/common/WEHeader/WEExtraHeader.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import useHeaderStore from '../../../stores/header/useHeaderStore';
+import useHeaderStore from '../../../stores/common/header/useHeaderStore';
 import { HeaderContainer, HeaderText } from './WEExtraHeader';
 
 const WEExtraHeader = ({ isExtraCarrot, text }) => {

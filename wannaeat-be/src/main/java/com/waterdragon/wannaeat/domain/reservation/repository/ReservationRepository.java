59fa7bc9ec79e86982144909759b20af0809f942
이경곤 <leeggon1213@naver.com>
2024-09-20 17:52:18 +0900

    feat: ReservationRepository countByUserAndRestaurant 메소드 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/repository/ReservationRepository.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/repository/ReservationRepository.java
@@ -3,6 +3,10 @@ package com.waterdragon.wannaeat.domain.reservation.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.waterdragon.wannaeat.domain.reservation.domain.Reservation;
+import com.waterdragon.wannaeat.domain.restaurant.domain.Restaurant;
+import com.waterdragon.wannaeat.domain.user.domain.User;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+	int countByUserAndRestaurant(User user, Restaurant restaurant);
 }

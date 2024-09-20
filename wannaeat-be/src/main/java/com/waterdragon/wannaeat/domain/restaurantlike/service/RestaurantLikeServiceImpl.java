2b7d510b213db9ff162d7e14452b17054ad12ac2
이경곤 <leeggon1213@naver.com>
2024-09-20 17:54:22 +0900

    feat: 찜 매장 목록 조회 Service 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurantlike/service/RestaurantLikeServiceImpl.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurantlike/service/RestaurantLikeServiceImpl.java
@@ -1,11 +1,20 @@
 package com.waterdragon.wannaeat.domain.restaurantlike.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.waterdragon.wannaeat.domain.reservation.repository.ReservationRepository;
 import com.waterdragon.wannaeat.domain.restaurant.domain.Restaurant;
+import com.waterdragon.wannaeat.domain.restaurant.domain.RestaurantImage;
 import com.waterdragon.wannaeat.domain.restaurant.exception.error.RestaurantNotFoundException;
+import com.waterdragon.wannaeat.domain.restaurant.repository.RestaurantImageRepository;
 import com.waterdragon.wannaeat.domain.restaurant.repository.RestaurantRepository;
 import com.waterdragon.wannaeat.domain.restaurantlike.domain.RestaurantLike;
+import com.waterdragon.wannaeat.domain.restaurantlike.dto.response.UserLikeDetailResponseDto;
+import com.waterdragon.wannaeat.domain.restaurantlike.dto.response.UserLikeListResponseDto;
 import com.waterdragon.wannaeat.domain.restaurantlike.exception.error.AlreadyLikeException;
 import com.waterdragon.wannaeat.domain.restaurantlike.exception.error.LikeNotFoundException;
 import com.waterdragon.wannaeat.domain.restaurantlike.repository.RestaurantLikeRepository;
@@ -22,6 +31,8 @@ public class RestaurantLikeServiceImpl implements RestaurantLikeService {
 	private final AuthUtil authUtil;
 	private final RestaurantRepository restaurantRepository;
 	private final RestaurantLikeRepository restaurantLikeRepository;
+	private final RestaurantImageRepository restaurantImageRepository;
+	private final ReservationRepository reservationRepository;
 
 	/**
 	 * 찜 등록 메소드
@@ -51,6 +62,49 @@ public class RestaurantLikeServiceImpl implements RestaurantLikeService {
 			.restaurant(restaurant)
 			.build();
 		restaurantLikeRepository.save(restaurantLike);
+	}
+
+	/**
+	 * 찜한 식당 목록 조회 메소드
+	 * TODO: 추후 JPQL이나 QueryDSL로 성능 개선해볼 것
+	 *
+	 * @return UserLikeResponseDto 조회한 매장 목록 정보
+	 */
+	@Override
+	public UserLikeListResponseDto getListUserLikeRestaurant() {
+
+		// 인증 회원 객체
+		User user = authUtil.getAuthenticatedUser();
+
+		List<UserLikeDetailResponseDto> userLikeDetailResponseDtos = new ArrayList<>();
+
+		// restaurant_likes 테이블에서 해당 user의 찜 식당을 모두 찾음.
+		List<RestaurantLike> restaurantLikes = restaurantLikeRepository.findByUser(user);
+		for (RestaurantLike restaurantLike : restaurantLikes) {
+			Restaurant restaurant = restaurantLike.getRestaurant();
+
+			Optional<RestaurantImage> restaurantImageUrlOptional = restaurantImageRepository.findTopByRestaurantOrderByImageIdAsc(restaurant);
+			String restaurantImageUrl = restaurantImageUrlOptional
+				.map(RestaurantImage::getImageUrl)
+				.orElse("");
+
+			int userReservationCnt = reservationRepository.countByUserAndRestaurant(user, restaurant);
+
+			UserLikeDetailResponseDto dto = UserLikeDetailResponseDto.builder()
+				.restaurantId(restaurant.getRestaurantId())
+				.restaurantName(restaurant.getName())
+				.restaurantDescription(restaurant.getDescription())
+				.restaurantImageUrl(restaurantImageUrl)
+				.restaurantLike(true) // 이미 좋아요 한 식당이므로 true 설정
+				.userReservationCnt(userReservationCnt)
+				.build();
+
+			userLikeDetailResponseDtos.add(dto);
+		}
+
+		return UserLikeListResponseDto.builder()
+			.userLikeDetailResponseDtos(userLikeDetailResponseDtos)
+			.build();
 	}
 
 	/**

0bbba630695ac6c32453e9503e3db3b62eee7f35
이경곤 <leeggon1213@naver.com>
2024-09-20 14:41:00 +0900

    feat: AlreadyLikeException 구현

---
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurantlike/exception/error/AlreadyLikeException.java
@@ -0,0 +1,7 @@
+package com.waterdragon.wannaeat.domain.restaurantlike.exception.error;
+
+public class AlreadyLikeException extends RuntimeException {
+	public AlreadyLikeException(String message) {
+		super(message);
+	}
+}

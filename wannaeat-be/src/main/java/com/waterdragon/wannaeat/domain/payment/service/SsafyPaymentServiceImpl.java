7eacf311d1e18d0d005c1dc1589415e8bb868067
이경곤 <leeggon1213@naver.com>
2024-09-22 14:12:16 +0900

    feat: 싸피페이 Service 클래스만 임시작성

---
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/payment/service/SsafyPaymentServiceImpl.java
@@ -0,0 +1,12 @@
+package com.waterdragon.wannaeat.domain.payment.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SsafyPaymentServiceImpl {
+}

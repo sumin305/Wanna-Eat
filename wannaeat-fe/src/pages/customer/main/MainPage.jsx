f72d618d87c4c27e9af0b33b84e04d182a15746a
sumin305 <gogotnals@naver.com>
2024-09-23 02:51:05 +0900

    feat: 손님 메인 화면 구현

--- wannaeat-fe/src/pages/customer/main/MainPage.jsx
+++
@@ -1,5 +0,0 @@
-const MainPage = () => {
-  return <div>손님 메인페이지</div>;
-};
-
-export default MainPage;
--- wannaeat-fe/src/route/routes.js
+++ wannaeat-fe/src/route/routes.js
@@ -9,7 +9,7 @@ import StatisticsPage from '../pages/manager/statistics/StatisticsPage';
 
 // 손님 관련 페이지
 import CustomerMainPage from '../pages/customer/main/MainPage';
-import MapRestaurantPage from '../pages/customer/map/MapRestaurantPage.jsx';
+import MapRestaurantPage from '../pages/customer/map/MapRestaurantPage';
 import ListPage from '../pages/customer/reservation/history/ListPage';
 import MyInfoPage from '../pages/customer/user/MyInfoPage';
 import TimeSelectPage from '../pages/customer/reservation/process/TimeSelectPage/TimeSelectPage.jsx';
@@ -46,10 +46,7 @@ const AppRoutes = () => {
           path="/customer/reservation/deposit-payment"
           element={<DepositPaymentPage />}
         />
-        <Route
-          path="/customer/reservation/success"
-          element={<SuccessPage/>}
-        />
+        <Route path="/customer/reservation/success" element={<SuccessPage />} />
         <Route path="/customer/reservationlist" element={<ListPage />} />
         <Route path="/customer/myinfo" element={<MyInfoPage />} />
       </Routes>

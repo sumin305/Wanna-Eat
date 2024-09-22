5ce4884e70da67b022a4fe290ea7d7fb14d263a5
곽예빈 <ferf99@naver.com>
2024-09-23 00:01:07 +0900

    feat: 회원정보 수정 service 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/user/service/UserService.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.waterdragon.wannaeat.domain.user.service;
 
 import com.waterdragon.wannaeat.domain.user.dto.request.PhoneCodeSendRequestDto;
 import com.waterdragon.wannaeat.domain.user.dto.request.PhoneCodeVerifyRequestDto;
+import com.waterdragon.wannaeat.domain.user.dto.request.UserEditRequestDto;
 import com.waterdragon.wannaeat.domain.user.dto.request.UserSignupRequestDto;
 import com.waterdragon.wannaeat.domain.user.dto.response.UserDetailResponseDto;
 
@@ -10,6 +11,8 @@ public interface UserService {
 	void signup(UserSignupRequestDto userSignupRequestDto);
 
 	UserDetailResponseDto getDetailMyUser();
+
+	void editUser(UserEditRequestDto userEditRequestDto);
 
 	boolean checkNicknameDuplicate(String nickname);
 

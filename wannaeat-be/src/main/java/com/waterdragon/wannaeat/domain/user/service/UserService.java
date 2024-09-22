62905f5562aba9deb84d0ac537034e92f50495a5
곽예빈 <ferf99@naver.com>
2024-09-22 21:52:07 +0900

    feat: 회원가입 service 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/user/service/UserService.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/user/service/UserService.java
@@ -2,8 +2,11 @@ package com.waterdragon.wannaeat.domain.user.service;
 
 import com.waterdragon.wannaeat.domain.user.dto.request.PhoneCodeSendRequestDto;
 import com.waterdragon.wannaeat.domain.user.dto.request.PhoneCodeVerifyRequestDto;
+import com.waterdragon.wannaeat.domain.user.dto.request.UserSignupRequestDto;
 
 public interface UserService {
+
+	void signup(UserSignupRequestDto userSignupRequestDto);
 
 	void sendPhoneAuthenticationCode(PhoneCodeSendRequestDto phoneCodeSendRequestDto);
 

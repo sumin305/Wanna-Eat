0e99847f6f4e23314d1396c36abb1abed89b0e2c
박소정 <thwjd0235@naver.com>
2024-09-24 13:45:38 +0900

    feat: 지도 페이지 헤더 구현, 필터링한 내용 필터링버튼으로 출력

--- wannaeat-fe/src/pages/customer/map/MapRestaurantPage.js
+++ wannaeat-fe/src/pages/customer/map/MapRestaurantPage.js
@@ -1,9 +1,83 @@
 import styled from '@emotion/styled';
+import theme from '../../../style/common/theme';
+
+const MapRestaurantBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  background: ${theme.color.primary};
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 10%;
+`;
+
+const SearchWrapper = styled.div`
+  position: relative;
+  background: ${theme.color.primary};
+  border-radius: 0 0 5px 5px;
+  display: flex;
+  top: 10%;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const SearchInput = styled.input`
+  width: 90vw;
+  height: 5vh;
+  padding-left: 0.3rem;
+  border-radius: 5px;
+  border: none;
+  font-size: ${theme.fontSize.px13};
+  ::placeholder {
+    color: ${theme.color.disabled};
+  }
+`;
+
+const SearchIcon = styled.img`
+  position: absolute;
+  right: 1.5rem;
+  height: 3vh;
+  cursor: pointer;
+  color: white;
+  transform: translateY(-50%);
+  margin-top: 15px;
+`;
 
 const MapBox = styled.div`
   display: flex;
   flex-direction: column;
-  height: 95%;
+  height: 80%;
 `;
 
-export { MapBox };
+const FilterButton = styled.span`
+  margin-top: 0.5rem;
+  background-color: ${theme.color.primary};
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: ${theme.fontSize.px13};
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row; // 버튼들을 한 줄로 정렬
+  gap: 0.5rem; // 버튼 간의 간격
+`;
+export {
+  MapRestaurantBox,
+  MapBox,
+  HeaderContainer,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  FilterButton,
+  ButtonContainer,
+};
--- wannaeat-fe/src/pages/customer/map/MapRestaurantPage.jsx
+++ wannaeat-fe/src/pages/customer/map/MapRestaurantPage.jsx
@@ -1,11 +1,25 @@
 import MapContainer from '../../../component/customer/map/Map.jsx';
 import useModalStore from '../../../stores/common/modal/useModalStore.js';
-import { MapBox } from './MapRestaurantPage';
+import {
+  MapRestaurantBox,
+  MapBox,
+  HeaderContainer,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  FilterButton,
+  ButtonContainer,
+} from './MapRestaurantPage';
 import MapFilterModalBox from '../../../component/customer/map/MapFilterModalBox.jsx';
+import searchIcon from '../../../assets/icons/common/search.svg';
+import useReservationStore from '../../../stores/customer/reservation/useReservationStore.js';
 
 const MapRestaurantPage = () => {
   const { open, setModalType, setTitle, setConfirmText, setChildren } =
     useModalStore();
+
+  const { selectedDate, selectedHeadCount, selectedCategory } =
+    useReservationStore();
 
   const handleFilterModalButtonClick = () => {
     setModalType('sheet');
@@ -15,11 +29,61 @@ const MapRestaurantPage = () => {
     open();
   };
 
+  // 버튼을 동적으로 생성하는 함수
+  const renderFilterButtons = () => {
+    const buttons = [];
+
+    // selectedDate가 있을 때 버튼 생성
+    if (selectedDate) {
+      buttons.push(
+        <FilterButton onClick={handleFilterModalButtonClick} key="date">
+          {selectedDate}
+        </FilterButton>
+      );
+    }
+
+    if (selectedHeadCount) {
+      buttons.push(
+        <FilterButton onClick={handleFilterModalButtonClick} key="headCount">
+          {selectedHeadCount}명
+        </FilterButton>
+      );
+    }
+
+    if (selectedCategory) {
+      buttons.push(
+        <FilterButton onClick={handleFilterModalButtonClick} key="category">
+          {selectedCategory}
+        </FilterButton>
+      );
+    }
+
+    return buttons;
+  };
+
+  const buttons = renderFilterButtons();
+
   return (
-    <MapBox>
-      <button onClick={handleFilterModalButtonClick}>필터링 모달 띄우기</button>
-      <MapContainer />
-    </MapBox>
+    <MapRestaurantBox>
+      <HeaderContainer>
+        <SearchWrapper>
+          <SearchInput placeholder="메뉴, 식당, 지역 검색" />
+          <SearchIcon src={searchIcon}></SearchIcon>
+        </SearchWrapper>
+        <ButtonContainer>
+          {buttons.length === 0 ? (
+            <FilterButton onClick={handleFilterModalButtonClick}>
+              검색 조건 설정하기
+            </FilterButton>
+          ) : (
+            buttons
+          )}
+        </ButtonContainer>
+      </HeaderContainer>
+      <MapBox>
+        <MapContainer />
+      </MapBox>
+    </MapRestaurantBox>
   );
 };
 

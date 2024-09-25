8a36439e0d2485ca2e5985b1eb1a0031cc58e3dd
sumin305 <gogotnals@naver.com>
2024-09-25 09:11:25 +0900

    feat: 토글 사용자 모드 변경 모드

--- wannaeat-fe/src/component/common/toggle/WEToggle.js
+++ wannaeat-fe/src/component/common/toggle/WEToggle.js
@@ -6,22 +6,39 @@ export const ToggleContainer = styled.div`
   cursor: pointer;
 `;
 
+export const ToggleText = styled.p`
+  font-size: ${theme.fontSize.px9};
+  color: white;
+  padding: 0 0.8rem;
+  position: absolute;
+  right: ${({ isOn }) => (isOn ? '2.9rem' : '0.3rem')};
+`;
+
 export const ToggleBackground = styled.div`
-  width: 2.125rem;
-  height: 1.063rem;
+  display: flex;
+  align-items: center;
+  width: 6rem;
+  height: 1.5rem;
   border-radius: 0.438rem;
-  background-color: ${({ isOn }) =>
-    isOn ? theme.color.primary : theme.color.disabled};
+  background-color: ${theme.color.disabled};
   transition: background-color 0.5s ease;
 `;
 
+export const ToggleCircleText = styled.p`
+  color: white;
+  font-size: ${theme.fontSize.px9};
+`;
 export const ToggleCircle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   position: absolute;
-  top: 0.063rem;
-  left: ${({ isOn }) => (isOn ? '1.125rem' : '0.063rem')};
-  width: 0.938rem;
-  height: 0.938rem;
-  border-radius: 50%;
-  background-color: ${theme.color.white};
+  top: 0;
+  left: ${({ isOn }) => (isOn ? '3rem' : '0.04rem')};
+  width: 3rem;
+  height: 1.5rem;
+  border-radius: 0.438rem;
+  background-color: ${theme.color.primary};
   transition: left 0.5s ease;
 `;

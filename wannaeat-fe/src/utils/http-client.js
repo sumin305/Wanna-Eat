c09c8096b40c07670aef8666e0c7afdbb0266628
sumin305 <gogotnals@naver.com>
2024-09-25 23:42:37 +0900

    feat: axios 헤더에 Authorization 추가한 instance 생성 함수 추가

--- wannaeat-fe/src/utils/http-client.js
+++ wannaeat-fe/src/utils/http-client.js
@@ -3,11 +3,11 @@ import Cookies from 'js-cookie';
 
 const createClientInstance = () => {
   const instance = axios.create({
-    baseURL: process.env.REACT_APP_REST_API_URL,
+    baseURL: process.env.REACT_APP_LOCAL_REST_API_URL,
     timeout: 5000,
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': process.env.REACT_APP_REST_API_URL,
+      'Access-Control-Allow-Origin': process.env.REACT_APP_CLIENT_URL,
       'Access-Control-Allow-Credentials': 'true',
     },
     withCredentials: true,
@@ -15,4 +15,40 @@ const createClientInstance = () => {
   return instance;
 };
 
+const createAuthClientInstance = (accessToken) => {
+  const instance = axios.create({
+    baseURL: process.env.REACT_APP_LOCAL_REST_API_URL,
+    timeout: 5000,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+      'Access-Control-Allow-Origin': process.env.REACT_APP_CLIENT_URL,
+      'Access-Control-Allow-Credentials': 'true',
+    },
+    withCredentials: true,
+  });
+
+  return instance;
+};
+
+const createAuthWithRefreshClientInstance = (accessToken) => {
+  const refreshToken = Cookies.get('refreshToken');
+
+  const instance = axios.create({
+    baseURL: process.env.REACT_APP_LOCAL_REST_API_URL,
+    timeout: 5000,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+      'Authorization-refresh': `Bearer ${refreshToken}`,
+      'Access-Control-Allow-Origin': process.env.REACT_APP_CLIENT_URL,
+      'Access-Control-Allow-Credentials': 'true',
+    },
+    withCredentials: true,
+  });
+
+  return instance;
+};
+
 export const clientInstance = createClientInstance();
+export { createAuthClientInstance, createAuthWithRefreshClientInstance };

e26728cba229888041d00344c87601368b947b95
박소정 <thwjd0235@naver.com>
2024-09-25 20:45:20 +0900

    feat: foregroundmessage, backgroundmessage 테스트 수신 성공

--- wannaeat-fe/src/index.js
+++ wannaeat-fe/src/index.js
@@ -4,6 +4,18 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+// 서비스 워커 등록 코드 추가
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>

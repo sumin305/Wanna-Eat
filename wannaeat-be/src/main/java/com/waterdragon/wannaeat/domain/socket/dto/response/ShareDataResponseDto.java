212681eb22d549b931bf281964cad8a6a6509ccc
이경곤 <leeggon1213@naver.com>
2024-09-26 15:06:37 +0900

    fix: ShareDataResponseDto 반환 타입 ChatMessageListResponseDto로 수정

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/socket/dto/response/ShareDataResponseDto.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/socket/dto/response/ShareDataResponseDto.java
@@ -3,6 +3,7 @@ package com.waterdragon.wannaeat.domain.socket.dto.response;
 import org.springframework.data.domain.Page;
 
 import com.waterdragon.wannaeat.domain.chatmessage.dto.response.ChatMessageDetailResponseDto;
+import com.waterdragon.wannaeat.domain.chatmessage.dto.response.ChatMessageListResponseDto;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -11,5 +12,5 @@ import lombok.Getter;
 @Builder
 public class ShareDataResponseDto {
 
-	Page<ChatMessageDetailResponseDto> chatMessageDetailResponseDtos;
+	ChatMessageListResponseDto chatMessageDetailResponseDtos;
 }

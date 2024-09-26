d655190b8a0f5836a2c0df7f9c4e0e570dfb86eb
이경곤 <leeggon1213@naver.com>
2024-09-26 14:12:39 +0900

    remove: Socket 패키지 -> ChatMessage 패키지 분리

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/socket/repository/ChatMessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.waterdragon.wannaeat.domain.socket.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.waterdragon.wannaeat.domain.socket.domain.ChatMessage;
-
-public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
-}

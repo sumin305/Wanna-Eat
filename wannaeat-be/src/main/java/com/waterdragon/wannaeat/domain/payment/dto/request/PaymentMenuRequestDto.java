310abf8af7c6ea702e79b2516530025366052655
곽예빈 <ferf99@naver.com>
2024-09-27 11:16:36 +0900

    rename: 직관적인 클래스명으로 변경

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/payment/dto/request/PaymentMenuRequestDto.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/payment/dto/request/PaymentMenuRequestDto.java
@@ -1,9 +1,6 @@
 package com.waterdragon.wannaeat.domain.payment.dto.request;
 
-import java.util.List;
-
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,12 +10,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class KakaoPaymentRequestDto {
+public class PaymentMenuRequestDto {
 
 	@NotNull
-	private Long reservationId;
-
+	private Long menuId;
 	@NotNull
-	@Size(min = 1, message = "결제 요청 내역은 비어있을 수 없습니다.")
-	private List<KakaoPaymentMenuRequestDto> kakaoPaymentMenuRequestDtos;
+	private Long orderId;
+	@NotNull
+	private Integer menuCount;
 }

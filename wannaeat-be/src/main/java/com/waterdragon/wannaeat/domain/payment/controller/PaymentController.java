d6dde490af2ed11befb8288aa1d0c8ba24b46876
곽예빈 <ferf99@naver.com>
2024-09-27 12:38:05 +0900

    fix: 비관적 락 유효성 검증 로직 service 단으로 이동

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/payment/controller/PaymentController.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/payment/controller/PaymentController.java
@@ -14,16 +14,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.waterdragon.wannaeat.domain.order.domain.Order;
-import com.waterdragon.wannaeat.domain.order.dto.request.OrderPaidCntEditRequestDto;
 import com.waterdragon.wannaeat.domain.order.repository.OrderRepository;
 import com.waterdragon.wannaeat.domain.order.service.OrderService;
 import com.waterdragon.wannaeat.domain.payment.dto.request.KakaoPaymentMenuRequestDto;
-import com.waterdragon.wannaeat.domain.payment.dto.request.KakaoPaymentRequestDto;
 import com.waterdragon.wannaeat.domain.payment.dto.response.KakaoPaymentApproveResponseDto;
 import com.waterdragon.wannaeat.domain.payment.dto.response.KakaoPaymentReadyResponseDto;
 import com.waterdragon.wannaeat.domain.payment.exception.error.InvalidPaymentException;
-import com.waterdragon.wannaeat.domain.payment.exception.error.MenuCountRequestMoreThanUnpaidException;
 import com.waterdragon.wannaeat.domain.payment.service.KakaoPaymentService;
 import com.waterdragon.wannaeat.global.redis.service.RedisService;
 import com.waterdragon.wannaeat.global.response.ResponseDto;
@@ -56,20 +52,20 @@ public class PaymentController {
 	@Operation(summary = "음식비 결제준비 요청 API(카카오페이)")
 	@PostMapping("/menus/ready/kakao")
 	public ResponseEntity<ResponseDto<KakaoPaymentReadyResponseDto>> kakaoPayReady(
-		@Valid @RequestBody KakaoPaymentRequestDto kakaoPaymentRequestDto) {
+		@Valid @RequestBody KakaoPaymentMenuRequestDto kakaoPaymentMenuRequestDto) {
 
 		// 랜덤한 payment_id 생성
 		String paymentId = UUID.randomUUID().toString();
 
 		// 카카오페이 최초 요청 및 결제창 연결
 		KakaoPaymentReadyResponseDto kakaoPaymentReadyResponseDto = kakaoPaymentService.kakaoPayReady(
-			kakaoPaymentRequestDto, paymentId);
+			kakaoPaymentMenuRequestDto, paymentId);
 
 		// 결제 요청 시 결제 고유번호 tid를 Redis에 저장 (key = paymentId, value = tid)
 		redisService.setValues(paymentId, kakaoPaymentReadyResponseDto.getTid(),
 			Duration.ofMillis(kakaopayTidExpirationMillis));
 		// 향후 수량 update 위해서 결제 요청 메뉴들도 Redis에 저장 (key = "payment_menus_{paymentId}", value = KakaoPaymentRequestDto)
-		redisService.setValues("payment_menus_" + paymentId, kakaoPaymentRequestDto,
+		redisService.setValues("payment_menus_" + paymentId, kakaoPaymentMenuRequestDto,
 			Duration.ofMillis(kakaopayTidExpirationMillis));
 
 		ResponseDto<KakaoPaymentReadyResponseDto> responseDto = ResponseDto.<KakaoPaymentReadyResponseDto>builder()
@@ -93,7 +89,8 @@ public class PaymentController {
 	@GetMapping("/completed/kakao")
 	public ResponseEntity<ResponseDto<KakaoPaymentApproveResponseDto>> kakaoPayApprove(
 		@RequestParam("payment_id") String paymentId,
-		@RequestParam("pg_token") String pgToken) {
+		@RequestParam("pg_token") String pgToken,
+		@RequestParam("type") String type) {
 
 		// LinkedHashMap을 KakaoPaymentRequestDto로 변환하기 위한 ObjectMapper 객체 생성
 		ObjectMapper objectMapper = new ObjectMapper();
@@ -103,44 +100,19 @@ public class PaymentController {
 
 		// 해당 paymentId에 대해서 Redis에 저장된 KakaoPaymentRequestDto 가져오기 (역직렬화 과정에서 LinkedHashMap으로 반환됨)
 		Object cachedObject = redisService.getValues("payment_menus_" + paymentId);
-		// LinkedHashMap을 KakaoPaymentRequestDto로 반환
-		KakaoPaymentRequestDto kakaoPaymentRequestDto = objectMapper.convertValue(cachedObject,
-			KakaoPaymentRequestDto.class);
 
-		// tid, KakaoPaymentRequestDto 유효성 확인
-		if (tid == null || tid.equals("false") || kakaoPaymentRequestDto == null) {
-			throw new InvalidPaymentException("유효하지 않은 결제 id 또는 결제 요청 정보가 존재하지 않습니다.");
-		}
+		if (type.equals("menu")) {
+			// LinkedHashMap을 KakaoPaymentMenuRequestDto로 반환
+			KakaoPaymentMenuRequestDto kakaoPaymentMenuRequestDto = objectMapper.convertValue(cachedObject,
+				KakaoPaymentMenuRequestDto.class);
 
-		// 누구든 결제할 수 있기 때문에 reservationParticipantId는 확인할 필요 없음.
-
-		// 실제 결제 진행
-		for (KakaoPaymentMenuRequestDto menuRequestDto : kakaoPaymentRequestDto.getKakaoPaymentMenuRequestDtos()) {
-
-			// 비관적 락을 활용한 (PESSIMISTIC_WRITE) 주문 Order 읽어오기
-			// 가장 상위 @Transactional 단위인 Controller의 kakayPayApprove 메소드 단위로 읽기,쓰기가 모두 불가능해진다.
-			// PESSIMISTIC_READ는 읽기는 가능함. 하지만 우리는 재고가 실시간으로 바뀌므로 읽기도 못하게 막아야 한다.
-			Order order = orderRepository.findByOrderIdWithLock(menuRequestDto.getOrderId())
-				.orElseThrow(() -> new IllegalArgumentException("해당 주문을 찾을 수 없습니다."));
-
-			// 결제 수량 확인 (남은 재고보다 더 많은 결제 요청이라면 결제로 안 넘어가게 함.)
-			int remainingCount = order.getTotalCnt() - order.getPaidCnt();
-			if (remainingCount < menuRequestDto.getMenuCount()) {
-				throw new MenuCountRequestMoreThanUnpaidException(
-					order.getMenu().getName() + "의 남은 미결제 수량보다 결제 수량이 많습니다.");
-				// HttpHeaders headers = new HttpHeaders();
-				// headers.setLocation(URI.create("/api/payments/fail/kakao"));  // fail URL로 리디렉션
-				// return new ResponseEntity<>(headers, HttpStatus.SEE_OTHER);  // 303 SEE_OTHER 리디렉션
+			// tid, KakaoPaymentRequestDto 유효성 확인
+			if (tid == null || tid.equals("false") || kakaoPaymentMenuRequestDto == null) {
+				throw new InvalidPaymentException("유효하지 않은 결제 id 또는 결제 요청 정보가 존재하지 않습니다.");
 			}
 
-			// OrderPaidCntEditRequestDto 생성
-			OrderPaidCntEditRequestDto orderPaidCntEditRequestDto = OrderPaidCntEditRequestDto.builder()
-				.orderId(menuRequestDto.getOrderId())
-				.paidMenuCnt(menuRequestDto.getMenuCount())
-				.build();
-
-			// 각 orderId의 paidCnt 수정
-			orderService.editOrderPaidCnt(orderPaidCntEditRequestDto);
+			// 누구든 결제할 수 있기 때문에 reservationParticipantId는 확인할 필요 없음.
+			kakaoPaymentService.menuPaymentValidCheck(kakaoPaymentMenuRequestDto);
 		}
 
 		// 카카오 결제 요청하기

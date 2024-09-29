62f5bd10779a7088d410ef5c0e75cc2b0c4d0ce4
곽예빈 <ferf99@naver.com>
2024-09-29 18:50:17 +0900

    feat: registerReservation 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/service/ReservationService.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/service/ReservationService.java
@@ -3,12 +3,16 @@ package com.waterdragon.wannaeat.domain.reservation.service;
 import java.util.List;
 
 import com.waterdragon.wannaeat.domain.reservation.domain.Reservation;
+import com.waterdragon.wannaeat.domain.reservation.dto.request.ReservationRegisterRequestDto;
 import com.waterdragon.wannaeat.domain.reservation.dto.request.UrlValidationRequestDto;
+import com.waterdragon.wannaeat.domain.reservation.dto.response.ReservationDetailResponseDto;
 import com.waterdragon.wannaeat.domain.reservation.dto.response.UrlValidationResponseDto;
 
 public interface ReservationService {
 
 	UrlValidationResponseDto validateUrl(UrlValidationRequestDto urlValidationRequestDto);
+
+	ReservationDetailResponseDto registerReservation(ReservationRegisterRequestDto reservationRegisterRequestDto);
 
 	void registerReservationTable(Reservation reservation, List<Integer> tableNumbers);
 

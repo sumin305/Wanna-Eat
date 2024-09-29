10d6819507474280f9e5206eea09a3d983ed2554
곽예빈 <ferf99@naver.com>
2024-09-30 01:08:54 +0900

    feat: countReservationsByDay 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/repository/ReservationRepository.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/repository/ReservationRepository.java
@@ -1,5 +1,6 @@
 package com.waterdragon.wannaeat.domain.reservation.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import com.waterdragon.wannaeat.domain.reservation.domain.Reservation;
+import com.waterdragon.wannaeat.domain.reservation.dto.response.ReservationCountResponseDto;
 import com.waterdragon.wannaeat.domain.restaurant.domain.Restaurant;
 import com.waterdragon.wannaeat.domain.user.domain.User;
 
@@ -29,4 +31,16 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
 	@Query("SELECT r FROM Reservation r JOIN FETCH r.restaurant WHERE r.reservationUrl = :reservationUrl")
 	Optional<Reservation> findByReservationUrlWithRestaurant(@Param("reservationUrl") String reservationUrl);
+
+	@Query(
+		"SELECT NEW com.waterdragon.wannaeat.domain.reservation.dto.response.ReservationCountResponseDto(DAY(r.reservationDate), COUNT(r)) "
+			+
+			"FROM Reservation r " +
+			"WHERE r.restaurant = :restaurant " +
+			"AND FUNCTION('YEAR', r.reservationDate) = :year " +
+			"AND FUNCTION('MONTH', r.reservationDate) = :month " +
+			"GROUP BY DAY(r.reservationDate)")
+	List<ReservationCountResponseDto> countReservationsByDay(@Param("restaurant") Restaurant restaurant,
+		@Param("year") int year,
+		@Param("month") int month);
 }

e7927c60db0f5103bc158278f1256879db0afc5e
곽예빈 <ferf99@naver.com>
2024-09-29 18:47:42 +0900

    feat: registerReservationTable 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/service/ReservationService.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/service/ReservationService.java
@@ -1,9 +1,15 @@
 package com.waterdragon.wannaeat.domain.reservation.service;
 
+import java.util.List;
+
+import com.waterdragon.wannaeat.domain.reservation.domain.Reservation;
 import com.waterdragon.wannaeat.domain.reservation.dto.request.UrlValidationRequestDto;
 import com.waterdragon.wannaeat.domain.reservation.dto.response.UrlValidationResponseDto;
 
 public interface ReservationService {
 
 	UrlValidationResponseDto validateUrl(UrlValidationRequestDto urlValidationRequestDto);
+
+	void registerReservationTable(Reservation reservation, List<Integer> tableNumbers);
+
 }
--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/service/ReservationServiceImpl.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/service/ReservationServiceImpl.java
@@ -1,12 +1,21 @@
 package com.waterdragon.wannaeat.domain.reservation.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.waterdragon.wannaeat.domain.reservation.domain.Reservation;
+import com.waterdragon.wannaeat.domain.reservation.domain.ReservationTable;
 import com.waterdragon.wannaeat.domain.reservation.dto.request.UrlValidationRequestDto;
 import com.waterdragon.wannaeat.domain.reservation.dto.response.UrlValidationResponseDto;
 import com.waterdragon.wannaeat.domain.reservation.exception.error.ReservationNotFoundException;
 import com.waterdragon.wannaeat.domain.reservation.repository.ReservationRepository;
+import com.waterdragon.wannaeat.domain.reservation.repository.ReservationTableRepository;
+import com.waterdragon.wannaeat.domain.restaurant.repository.RestaurantRepository;
+import com.waterdragon.wannaeat.domain.restaurant.repository.RestaurantStructureRepository;
+import com.waterdragon.wannaeat.domain.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +25,17 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ReservationServiceImpl implements ReservationService {
 
+	private final RestaurantStructureRepository restaurantStructureRepository;
+	@Value("${redirectURL}")
+	private String REDIRECT_URL;
+
+	@Value("${RESERVATION_URL}")
+	private String RESERVATION_URL;
+
 	private final ReservationRepository reservationRepository;
+	private final RestaurantRepository restaurantRepository;
+	private final UserRepository userRepository;
+	private final ReservationTableRepository reservationTableRepository;
 
 	@Override
 	public UrlValidationResponseDto validateUrl(UrlValidationRequestDto urlValidationRequestDto) {
@@ -31,4 +50,23 @@ public class ReservationServiceImpl implements ReservationService {
 			.reservationId(reservation.getReservationId())
 			.build();
 	}
+
+	/**
+	 * 예약 테이블 등록 메소드
+	 *
+	 * @param reservation 대상 예약
+	 * @param tableNumbers 예약 테이블 목록
+	 */
+	@Override
+	@Transactional
+	public void registerReservationTable(Reservation reservation, List<Integer> tableNumbers) {
+		for (Integer tableNumber : tableNumbers) {
+			reservationTableRepository.saveAndFlush(ReservationTable.builder()
+				.reservation(reservation)
+				.tableId(tableNumber)
+				.build());
+			log.info("테이블등록" + tableNumber);
+		}
+	}
+
 }

06e669d8902fcaa718594926ba211efff63a2ffe
이경곤 <leeggon1213@naver.com>
2024-09-29 13:03:45 +0900

    feat: CartMenuRegisterResponseDto 구현

---
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/cart/dto/response/CartMenuRegisterResponseDto.java
@@ -0,0 +1,15 @@
+package com.waterdragon.wannaeat.domain.cart.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CartMenuRegisterResponseDto {
+
+	private String menuName;
+	private String menuImage;
+	private int menuPrice;
+	private int menuCnt;
+	private int menuTotalPrice;
+}

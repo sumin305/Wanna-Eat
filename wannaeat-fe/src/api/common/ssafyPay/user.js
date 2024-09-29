ad1fac231a5566056507f3128077308569657430
이수민 <gogotnals@naver.com>
2024-09-29 22:43:57 +0900

    feat: 사용자 계정 조회 api 구현

--- wannaeat-fe/src/api/common/ssafyPay/user.js
+++ wannaeat-fe/src/api/common/ssafyPay/user.js
@@ -15,3 +15,19 @@ export const createSsafyPayAccount = async (userId) => {
     })
     .catch((error) => error);
 };
+
+// 사용자 계정 조회
+export const getSsafyPayAccount = async (userId) => {
+  return await ssafyClient
+    .post('/api/v1/member/search', {
+      apiKey: process.env.REACT_APP_SSAFY_PAY_API_KEY,
+      userId: userId,
+    })
+    .then((result) => {
+      const userKey = result.data.userKey;
+      localStorage.setItem('userKey', userKey);
+      console.log('userKey: ', userKey);
+      return result;
+    })
+    .catch((error) => error);
+};

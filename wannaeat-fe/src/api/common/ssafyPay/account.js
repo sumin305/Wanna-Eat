93800e77cb3e6fdb473d3e76ced2ba272cb1ab82
이수민 <gogotnals@naver.com>
2024-09-29 22:58:28 +0900

    feat: 계좌 상품 리스트 조회 api 구현

---
+++ wannaeat-fe/src/api/common/ssafyPay/account.js
@@ -0,0 +1,33 @@
+import { ssafyClient } from '../../../utils/http-client';
+import moment from 'moment';
+
+const currentDate = moment(new Date()).format('YYYYMMDD');
+const currentTime = moment(new Date()).format('HHmmss');
+
+const Header = (apiName, userKey = '') => {
+  const randomTempSevenNumber = '000' + (Math.random(999999) + 1);
+  const randomFourNumber = randomTempSevenNumber.slice(-6);
+  const institutionTransactionUniqueNo =
+    moment(new Date()).format('YYYYMMDDHHmmss') + randomFourNumber;
+  return {
+    apiName: apiName,
+    transmissionDate: currentDate,
+    transmissionTime: currentTime,
+    institutionCode: '00100',
+    fintechAppNo: '001',
+    apiServiceCode: apiName,
+    institutionTransactionUniqueNo: institutionTransactionUniqueNo,
+    apiKey: process.env.REACT_APP_SSAFY_PAY_API_KEY,
+    ...(userKey !== '' && { userKey: userKey }),
+  };
+};
+
+// 계좌 리스트 조회
+export const getAccountList = async () => {
+  return await ssafyClient.post(
+    '/api/v1/edu/demandDeposit/inquireDemandDepositList',
+    {
+      Header: Header('inquireDemandDepositList'),
+    }
+  );
+};

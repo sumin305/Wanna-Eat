7a483bf0f2b44f6b1107fe2a22c3e24de826fa9e
곽예빈 <ferf99@naver.com>
2024-09-29 18:43:19 +0900

    fix: findReservedTables 로직 수정

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/repository/ReservationTableRepository.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/repository/ReservationTableRepository.java
@@ -17,8 +17,9 @@ public interface ReservationTableRepository extends JpaRepository<ReservationTab
 		"JOIN rt.reservation r " +
 		"WHERE r.restaurant.restaurantId = :restaurantId " +
 		"AND r.reservationDate = :reservationDate " +
-		"AND r.endTime > :startTime " +
-		"AND r.startTime < :endTime " +
+		"AND ((r.endTime > :startTime AND r.startTime < :endTime) " +  // 겹치는 경우 필터링
+		"OR (r.endTime = :startTime) " +  // 기존 예약의 종료 시간이 내 예약의 시작 시간과 같을 때 허용
+		"OR (r.startTime = :endTime)) " + // 기존 예약의 시작 시간이 내 예약의 종료 시간과 같을 때 허용
 		"AND r.cancelled = false")
 		// 해당 식당의 검색 startTime ~ 검색 endTime의 모든 예약 테이블 정보들을 모두 불러온다. 겹치는 애들도 다 불러옴.
 	List<ReservationTable> findReservedTables(

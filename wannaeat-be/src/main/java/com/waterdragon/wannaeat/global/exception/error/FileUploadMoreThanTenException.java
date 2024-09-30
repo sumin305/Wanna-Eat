4e84a596959b4e1d2f3a486e71bac42fd51e2a4d
이경곤 <leeggon1213@naver.com>
2024-09-30 23:24:45 +0900

    feat: FileUploadMoreThanTenException 구현

---
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/global/exception/error/FileUploadMoreThanTenException.java
@@ -0,0 +1,7 @@
+package com.waterdragon.wannaeat.global.exception.error;
+
+public class FileUploadMoreThanTenException extends RuntimeException {
+	public FileUploadMoreThanTenException(String message) {
+		super(message);
+	}
+}

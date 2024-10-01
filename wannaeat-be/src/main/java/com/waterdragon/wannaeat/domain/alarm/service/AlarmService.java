108be8b69d8a5da2605b20bf11ee75d6791fbc70
wjd47 <wjd4782@naver.com>
2024-10-01 22:25:06 +0900

    feat: 알림 조회 서비스 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/alarm/service/AlarmService.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/alarm/service/AlarmService.java
@@ -1,8 +1,13 @@
 package com.waterdragon.wannaeat.domain.alarm.service;
 
 import com.waterdragon.wannaeat.domain.alarm.dto.request.AlarmRegisterRequestDto;
+import com.waterdragon.wannaeat.domain.alarm.dto.response.AlarmGetResponseDto;
+
+import java.util.List;
 
 public interface AlarmService {
 
 	void registerAlarm(AlarmRegisterRequestDto alarmRegisterRequestDto);
+
+	List<AlarmGetResponseDto> getListAlarmByUserId(Long userId);
 }

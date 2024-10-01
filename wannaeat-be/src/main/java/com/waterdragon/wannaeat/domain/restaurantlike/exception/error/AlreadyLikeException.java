835381e232ddd10c9a99be804e8c132c5504bf37
이경곤 <leeggon1213@naver.com>
2024-10-01 11:52:55 +0900

    rename: LikeDuplicateException 예외명 수정

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurantlike/exception/error/AlreadyLikeException.java
+++
@@ -1,7 +0,0 @@
-package com.waterdragon.wannaeat.domain.restaurantlike.exception.error;
-
-public class AlreadyLikeException extends RuntimeException {
-	public AlreadyLikeException(String message) {
-		super(message);
-	}
-}

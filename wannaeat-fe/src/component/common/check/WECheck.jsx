19e9dfe99bfec9a6104ad0afde2e6e3fe700c42b
박소정 <thwjd0235@naver.com>
2024-10-02 02:25:30 +0900

    feat: 체크버튼 onClick기능 추가

--- wannaeat-fe/src/component/common/check/WECheck.jsx
+++ wannaeat-fe/src/component/common/check/WECheck.jsx
@@ -3,9 +3,9 @@ import { CheckContainer, CheckImg } from './WECheck';
 import CheckPrimary from '../../../assets/icons/common/check-primary.svg';
 import CheckWhite from '../../../assets/icons/common/check-white.svg';
 
-const WECheck = ({ isChecked }) => {
+const WECheck = ({ isChecked, onClick }) => {
   return (
-    <div>
+    <div onClick={onClick}>
       <CheckContainer isChecked={isChecked}>
         <CheckImg src={CheckWhite} alt="하얀색 체크" />
       </CheckContainer>

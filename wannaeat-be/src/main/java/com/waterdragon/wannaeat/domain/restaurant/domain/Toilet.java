a60e18bb0f2f60c7b04a9b069bf4c8c43866ed05
이경곤 <leeggon1213@naver.com>
2024-10-01 11:03:48 +0900

    fix: Toilet 필드 수정

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurant/domain/Toilet.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurant/domain/Toilet.java
@@ -7,12 +7,7 @@ import lombok.Getter;
 @Builder
 public class Toilet {
 
-	private int toiletId; // 화장실 번호
-	private double ratio; // 가로,세로 비율
-	private double x; // X 좌표
-	private double y; // Y 좌표
-	private double angle; // 각도
-	private double length; // 화장실 길이
-	private double width; // 화장실 너비
-	private int floor; // 화장실이 위치한 층
+	private double x;
+	private double y;
+	private int floor;
 }

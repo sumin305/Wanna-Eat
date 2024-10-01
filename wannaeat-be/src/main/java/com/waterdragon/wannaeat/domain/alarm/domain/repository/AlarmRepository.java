1dc89d09ff6b8034023bb71f56acb8dfdcfad0fd
wjd47 <wjd4782@naver.com>
2024-10-01 22:24:51 +0900

    feat: 알림 조회 쿼리 작성

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/alarm/domain/repository/AlarmRepository.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/alarm/domain/repository/AlarmRepository.java
@@ -3,13 +3,28 @@ package com.waterdragon.wannaeat.domain.alarm.domain.repository;
 import java.util.List;
 import java.util.Optional;
 
+import com.waterdragon.wannaeat.domain.alarm.dto.response.AlarmGetResponseDto;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.waterdragon.wannaeat.domain.alarm.domain.Alarm;
 import com.waterdragon.wannaeat.domain.user.domain.User;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
-	Optional<List<Alarm>> findByUser(User user);
+    Optional<List<Alarm>> findByUser(User user);
 
+    @Query("SELECT new com.waterdragon.wannaeat.domain.alarm.dto.response.AlarmGetResponseDto(" +
+            "a.type, " +
+            "a.reservation.reservationId, " +
+            "COALESCE(a.menu.name, NULL), " +
+            "COALESCE(a.menu.image, 'https://wannaeat.s3.ap-northeast-2.amazonaws.com/df11e06e-2fb3-4315-8ce3-bc472362fadf-logo.png'), " +
+            "a.reservation.memberCnt, " +  // 예약 인원 추가
+            "a.registTime) " +
+            "FROM Alarm a " +
+            "LEFT JOIN a.menu " +
+            "WHERE a.user.userId = :userId " +
+            "ORDER BY a.registTime DESC")
+    List<AlarmGetResponseDto> findAlarmsByUserId(@Param("userId") Long userId);
 }

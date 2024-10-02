d916047a6c8dfe5c562dd9a46ed731db99038351
이경곤 <leeggon1213@naver.com>
2024-10-02 17:50:22 +0900

    feat: ReservationService validateReservationUrl 쿠키 검증 및 새로운 닉네임 생성 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/service/ReservationService.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/reservation/service/ReservationService.java
@@ -18,7 +18,7 @@ import com.waterdragon.wannaeat.domain.restaurant.domain.Restaurant;
 
 public interface ReservationService {
 
-	UrlValidationResponseDto validateUrl(UrlValidationRequestDto urlValidationRequestDto);
+	UrlValidationResponseDto validateUrl(UrlValidationRequestDto urlValidationRequestDto, String participantIdFromCookie);
 
 	ReservationDetailResponseDto registerReservation(ReservationRegisterRequestDto reservationRegisterRequestDto);
 

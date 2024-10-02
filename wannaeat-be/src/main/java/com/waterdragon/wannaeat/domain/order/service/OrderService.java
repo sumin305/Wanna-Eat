898d900aae372680230bc92186df0962c76cc33d
이경곤 <leeggon1213@naver.com>
2024-10-02 14:52:46 +0900

    feat: OrderService getListOrderByReservationUrl 예약 url로 주문서 목록 가져오기 메소드 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/order/service/OrderService.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/order/service/OrderService.java
@@ -2,10 +2,13 @@ package com.waterdragon.wannaeat.domain.order.service;
 
 import com.waterdragon.wannaeat.domain.order.dto.request.OrderPaidCntEditRequestDto;
 import com.waterdragon.wannaeat.domain.order.dto.request.OrderRegisterRequestDto;
+import com.waterdragon.wannaeat.domain.order.dto.response.OrderListResponseDto;
 
 public interface OrderService {
 
 	void editOrderPaidCnt(OrderPaidCntEditRequestDto orderPaidCntEditRequestDto);
 
 	void registerOrder(OrderRegisterRequestDto orderRegisterRequestDto);
+
+	OrderListResponseDto getListOrderByReservationUrl(String reservationUrl);
 }

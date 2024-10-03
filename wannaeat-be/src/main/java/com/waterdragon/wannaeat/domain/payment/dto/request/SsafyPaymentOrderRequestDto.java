ddd2829aa5bd00cda3ab6858e3515471baad0fc9
이경곤 <leeggon1213@naver.com>
2024-10-04 04:30:58 +0900

    feat: SsafyPaymentOrderRequestDto 구현

---
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/payment/dto/request/SsafyPaymentOrderRequestDto.java
@@ -0,0 +1,25 @@
+package com.waterdragon.wannaeat.domain.payment.dto.request;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SsafyPaymentOrderRequestDto extends SsafyPaymentRequestDto {
+
+	@NotEmpty(message = "예약 url은 필수 입력값입니다.")
+	private String reservationUrl;
+
+	@NotNull
+	@Size(min = 1, message = "결제 요청 내역은 비어있을 수 없습니다.")
+	private List<PaymentMenuRequestDto> paymentMenuRequestDtos;
+}

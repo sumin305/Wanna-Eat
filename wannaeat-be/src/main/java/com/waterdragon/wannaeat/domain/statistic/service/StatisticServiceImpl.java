00b9f2fc48908adb9f3b8bfbe9e9b20de2b56d58
곽예빈 <ferf99@naver.com>
2024-10-03 22:02:49 +0900

    feat: 예외처리 추가

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/statistic/service/StatisticServiceImpl.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/statistic/service/StatisticServiceImpl.java
@@ -27,10 +27,13 @@ import com.waterdragon.wannaeat.domain.reservation.repository.ReservationReposit
 import com.waterdragon.wannaeat.domain.restaurant.domain.Restaurant;
 import com.waterdragon.wannaeat.domain.restaurant.domain.RestaurantStructure;
 import com.waterdragon.wannaeat.domain.restaurant.domain.Table;
+import com.waterdragon.wannaeat.domain.restaurant.exception.error.RestaurantStructureNotFoundException;
 import com.waterdragon.wannaeat.domain.restaurant.repository.RestaurantStructureRepository;
 import com.waterdragon.wannaeat.domain.statistic.dto.response.MainStatisticResponseDto;
 import com.waterdragon.wannaeat.domain.statistic.dto.response.MenuStatisticResponseDto;
 import com.waterdragon.wannaeat.domain.statistic.dto.response.PeekStatisticResponseDto;
+import com.waterdragon.wannaeat.domain.statistic.dto.response.RevenuePerDayResponseDto;
+import com.waterdragon.wannaeat.domain.statistic.dto.response.RevenueStatisticResponseDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -396,7 +399,8 @@ public class StatisticServiceImpl implements StatisticService {
 	@Override
 	public double getTurnoverRate(Restaurant restaurant, List<Reservation> reservations) {
 		RestaurantStructure restaurantStructure = restaurantStructureRepository.findByRestaurantId(
-			restaurant.getRestaurantId()).get();
+				restaurant.getRestaurantId())
+			.orElseThrow(() -> new RestaurantStructureNotFoundException("매장 구조 정보가 존재하지 않습니다."));
 
 		List<Table> restaurantTables = restaurantStructure.getTables();
 		// 해당 매장의 테이블 수
@@ -441,7 +445,7 @@ public class StatisticServiceImpl implements StatisticService {
 
 	/**
 	 * 예약 목록 중 고유 예약 일자 수를 리턴하는 메소드
-	 * @param reservations
+	 * @param reservations 예약 목록
 	 * @return 고유 예약 일자 수
 	 */
 	@Override
@@ -460,7 +464,7 @@ public class StatisticServiceImpl implements StatisticService {
 	/**
 	 * 예약 목록의 예약 테이블 수의 합을 리턴하는 메소드
 	 *
-	 * @param reservations
+	 * @param reservations 예약 목록
 	 * @return 예약 테이블 수 합
 	 */
 	@Override

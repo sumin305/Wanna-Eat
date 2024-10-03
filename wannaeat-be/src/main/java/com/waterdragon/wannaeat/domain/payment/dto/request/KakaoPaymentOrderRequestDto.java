7d51ae32dedde42e0bf063261a048fd5e19399e2
이경곤 <leeggon1213@naver.com>
2024-10-04 04:25:09 +0900

    fix: KakaoPaymentOrderRequestDto reservationUrl 필드 변경

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/payment/dto/request/KakaoPaymentOrderRequestDto.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/payment/dto/request/KakaoPaymentOrderRequestDto.java
@@ -2,6 +2,7 @@ package com.waterdragon.wannaeat.domain.payment.dto.request;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,10 +14,10 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class KakaoPaymentMenuRequestDto {
+public class KakaoPaymentOrderRequestDto {
 
-	@NotNull
-	private Long reservationId;
+	@NotEmpty(message = "예약 url은 필수 입력값입니다.")
+	private String reservationUrl;
 
 	@NotNull
 	@Size(min = 1, message = "결제 요청 내역은 비어있을 수 없습니다.")

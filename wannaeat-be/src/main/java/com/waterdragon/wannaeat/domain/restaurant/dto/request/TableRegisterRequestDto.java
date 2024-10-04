a939a7bca4a3923b178ee35d7f47fdbe8265921c
이경곤 <leeggon1213@naver.com>
2024-10-04 12:14:47 +0900

    feat: TableRegisterRequestDto 구현

--- wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurant/dto/request/TableRegisterRequestDto.java
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/restaurant/dto/request/TableRegisterRequestDto.java
@@ -1,5 +1,6 @@
 package com.waterdragon.wannaeat.domain.restaurant.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,14 +9,24 @@ import lombok.Getter;
 @Builder
 public class TableRegisterRequestDto {
 
-	@NotNull(message = "테이블 번호는 필수 입력값입니다.")
-	private Integer tableId;
-	@NotNull(message = "테이블 인원 수는 필수 입력값입니다.")
-	private Integer assignedSeats;
-	@NotNull(message = "테이블 x좌표는 필수 입력값입니다.")
+	@NotEmpty(message = "아이템 id(UUID)는 필수 입력값입니다.")
+	private String itemId;
+
+	@NotEmpty(message = "아이템 타입은 필수 입력값입니다. (COUNTER, RESTROOM, ENTRANCE, KITCHEN, SQUARE, ROUNDED")
+	private String itemType;
+
+	@NotNull(message = "x 좌표는 필수 입력값입니다.")
 	private Double x;
-	@NotNull(message = "테이블 y좌표는 필수 입력값입니다.")
+
+	@NotNull(message = "y 좌표는 필수 입력값입니다.")
 	private Double y;
-	@NotNull(message = "테이블 층수는 필수 입력값입니다.")
+
+	@NotNull(message = "층수는 필수 입력값입니다.")
 	private Integer floor;
+
+	// 테이블 타입일 때만 필요한 필드들
+	@NotNull(message = "테이블 id는 필수 입력값입니다.")
+	private Integer tableId;
+	@NotNull(message = "좌석 수는 필수 입력값입니다.")
+	private Integer assignedSeats;
 }

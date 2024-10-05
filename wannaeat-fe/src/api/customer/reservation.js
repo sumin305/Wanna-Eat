f082396c18dacfda71d4121df3c5a043ce252b07
이수민 <gogotnals@naver.com>
2024-10-05 21:20:56 +0900

    feat: reservation api 연결

--- wannaeat-fe/src/api/customer/reservation.js
+++ wannaeat-fe/src/api/customer/reservation.js
@@ -1,8 +1,25 @@
 import { authClientInstance } from '../../utils/http-client';
 
+// 나의 예약 목록 조회
 export const getMyReservation = async () => {
   return await authClientInstance
     .get('/api/users/reservations')
     .then((result) => result.data)
+    .catch((error) => error);
+};
+
+// 예약 상세 조회
+export const getReservationDetail = async (reservationId) => {
+  return await authClientInstance
+    .get(`/api/users/reservations/${reservationId}`)
+    .then((result) => result)
+    .catch((error) => error);
+};
+
+// 예약 취소
+export const cancelReservation = async (reservationId) => {
+  return await authClientInstance
+    .delete('/api/reservations/' + reservationId)
+    .then((result) => result)
     .catch((error) => error);
 };

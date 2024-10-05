d485c3059179d9e221c21e2acd57dcff81395f06
wjd47 <wjd4782@naver.com>
2024-10-05 21:02:07 +0900

    feat: 예약 관련 api 수정(사업자)

--- wannaeat-fe/src/api/manager/reservation/reservation.js
+++ wannaeat-fe/src/api/manager/reservation/reservation.js
@@ -19,3 +19,21 @@ export const getReservationInfoByMonth = async () => {
     .then((result) => result.data)
     .catch((error) => error);
 };
+
+// 예약 상세 조회
+export const getReservationDetail = async (reservationId) => {
+  return await authClientInstance
+    .get(`/api/manager/reservation/${reservationId}`)
+    .then((result) => result.data)
+    .catch((error) => {});
+};
+
+// 서빙 처리 API 호출 함수
+export const serveOrder = async (reservationId, orderIdList) => {
+  return await authClientInstance
+    .post(`/api/reservation/${reservationId}/orders/serve`, {
+      orderIdList: orderIdList, // 배열 형식으로 전달
+    })
+    .then((result) => result)
+    .catch((error) => error);
+};

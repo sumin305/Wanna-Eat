96cc6d65bcaa032b330ffed2737ef0fb4e0839dc
곽예빈 <ferf99@naver.com>
2024-10-05 16:10:21 +0900

    feat: InvalidPaymentPasswordException 구현

---
+++ wannaeat-be/src/main/java/com/waterdragon/wannaeat/domain/payment/exception/error/InvalidPaymentPasswordException.java
@@ -0,0 +1,7 @@
+package com.waterdragon.wannaeat.domain.payment.exception.error;
+
+public class InvalidPaymentPasswordException extends RuntimeException {
+	public InvalidPaymentPasswordException(String message) {
+		super(message);
+	}
+}

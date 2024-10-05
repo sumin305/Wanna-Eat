3dcd2cb404777a091209b622f3acb006b9f98f26
wjd47 <wjd4782@naver.com>
2024-10-05 21:00:57 +0900

    remove: 필요 없는 파일 삭제

--- wannaeat-fe/src/api/manager/alarm.js
+++
@@ -1,8 +0,0 @@
-import { clientInstance, authClientInstance } from '../../utils/http-client';
-
-export const getAlarms = async () => {
-    return await authClientInstance
-      .get('/api/alarms')
-      .then((result) => result)
-      .catch((error) => error);
-  };

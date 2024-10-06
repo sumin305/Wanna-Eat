4476afc6f0ef9bd44e2e94e6c5c30632937bdeab
sumin305 <gogotnals@naver.com>
2024-10-06 14:06:23 +0900

    feat: 최우선 방문 예약 식당 조회 기능

--- wannaeat-fe/src/pages/customer/reservation/history/ReservationListPage/ReservationListPage.js
+++ wannaeat-fe/src/pages/customer/reservation/history/ReservationListPage/ReservationListPage.js
@@ -94,7 +94,7 @@ const ReservationAlertTime = styled.p`
 const ReservationLastTime = styled.p`
   color: ${theme.color.primary};
   font-weight: 900;
-  font-size: 1.8rem;
+  font-size: 1.7rem;
   margin-right: 0.5rem;
 
   @media (min-width: 480px) {
